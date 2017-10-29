39bd6869fd33939642b1329e5d9f6b1ff0701402
maxmya <maxmya@outlook.com>
2017-10-29 16:50:30 +0200

    init

---
+++ js/firebaseInit.js
@@ -0,0 +1,10 @@
+
+var config = {
+apiKey: "AIzaSyCEMK-eaZdGpWfn7FO2FBq_7OShiN5EcUg",
+authDomain: "projectwadeeny.firebaseapp.com",
+databaseURL: "https://projectwadeeny.firebaseio.com",
+projectId: "projectwadeeny",
+storageBucket: "projectwadeeny.appspot.com",
+messagingSenderId: "1072373959063"
+};
+firebase.initializeApp(config);
